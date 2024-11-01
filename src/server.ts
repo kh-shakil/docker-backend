ee109515e317f21361655aed7b6ba2cfacaac800
T45+Hx[lO7mVR@h <shakil@primotechltd.com>
2024-11-01 19:12:11 +0600

    module-4

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import { Server } from "http";
+import mongoose from "mongoose";
 import app from "./app";
+import config from "./app/config";
 import { errorlogger, logger } from "./app/src/shared/logger";
 
 let server: Server;
 
 async function main() {
   try {
+    await mongoose.connect(config.database_url as string);
+
+    logger.info("Connected to database");
+
+
     server = app.listen(process.env.PORT, () => {
       console.log(`app is listening on port ${process.env.PORT}`);
       logger.info(`app is listening on port ${process.env.PORT}`);
